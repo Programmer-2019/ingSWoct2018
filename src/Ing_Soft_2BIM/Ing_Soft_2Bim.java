54864fff2bc5f1b8aafca60dd657efb6b4cd8a6f
Programmer-2019 <52929763+Programmer-2019@users.noreply.github.com>
2019-07-15 16:43:57 -0500

    Add files via upload
    AL ANTERIOR ARCHIVO FUENTE SE LE ADICIO LA OPCION DE COTIZACION DE VEHICULOS PARA COMPLETAR LOS REQUERIMIENTOS TOTALES QUE SOLICITA EL PROGRAMA

--- src/Ing_Soft_2BIM/Ing_Soft_2Bim.java
+++ src/Ing_Soft_2BIM/Ing_Soft_2Bim.java
@@ -4,10 +4,12 @@ public class Ing_Soft_2Bim
 {
     public static void main(String[] args)
     {
+        
+        
         //objeto que despliega menu de opciones
         MenuOpciones menu=new MenuOpciones();
         menu.desplegarMenu();
-       
+        //aplicacion.ingresoDatos();
     }
    
 }
